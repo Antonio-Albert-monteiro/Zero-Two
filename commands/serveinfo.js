01e3a1a82118bc40b9d30cc2bcbb661f604ab892
Antonio Albert <antonioalbert349@gmail.com>
2021-04-24 16:03:34 -0300

    update serveinfo.js

--- commands/serveinfo.js
+++ commands/serveinfo.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
     .addField(`:closed_lock_with_key: Server Verification Level`, message.guild.verificationLevel, true)
     .addField(':man: Membros', `${message.guild.members.cache.filter(member => member.user.bot).size} Bots | ${(message.guild.memberCount) - (message.guild.members.cache.filter(member => member.user.bot).size)} Humanos | ${message.guild.memberCount} Total de Membros | ${Math.round((message.guild.members.cache.filter(member => member.user.bot).size / message.guild.memberCount) * 100)}% Bots | ${Math.round((((message.guild.memberCount) - (message.guild.members.cache.filter(member => member.user.bot).size)) / message.guild.memberCount) * 100)}% Humanos`, true)
     .addField(':wrench:  Canais', `${message.guild.channels.cache.filter(chan => chan.type === 'voice').size} Chats de Voz | ${message.guild.channels.cache.filter(chan => chan.type === 'text').size} Canais de Texto | ${message.guild.channels.cache.filter(chan => chan.type === 'category').size} Categorias | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'voice').size / message.guild.channels.cache.size) * 100)}% Chats de Voz | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'text').size / message.guild.channels.cache.size) * 100)}% Canais de Texto | ${Math.round((message.guild.channels.cache.filter(chan => chan.type === 'category').size / message.guild.channels.cache.size) * 100)}% Categorias`, true)
-    .addField(":inbox_tray:  Você Entrou", message.member.joinedAt)
+    .addField(":inbox_tray:  Você Entrou", message.member.toLocaleDateString("pt-BR"))
     .setFooter(` • Autor ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
 
     message.channel.send(serverembed);
