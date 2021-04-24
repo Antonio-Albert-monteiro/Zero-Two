f7e04a7c80c0fb314b34e2074e43bb5157e8d12c
Antonio Albert <antonioalbert349@gmail.com>
2021-04-24 17:10:11 -0300

    update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -5,7 +5,7 @@ exports.run = async (message, args) => {
     .setTitle("Central de comandos")
     .setDescription("
         escolha a cartegoria:
-     🛠 moderação
+     🛠 Moderação
 	 💰 Economia
 	 📡 Info
 	 😂 Diversão
@@ -23,6 +23,43 @@ exports.run = async (message, args) => {
 		let filtro3 = (r, u) => r.emoji.name === '📡' && u.id === message.author.id;
 		let filtro4 = (r, u) => r.emoji.name === '😂' && u.id === message.author.id;
 		
+		let coletor = msg.createReactionCollector(filtro1);
+		let coletor1 = msg.createReactionCollector(filtro2);
+		let coletor2 = msg.createReactionCollector(filtro3);
+		let coletor3 = msg.createReactionCollector(filtro4);
 		
+		coletor.on('collect', c => {
+		    let embed = new Discord.MessageEmbed()
+		    .setTitle("Central de comandos")
+		    .setDescription("Moderação")
+		    .setColor('#8A2BE2')
+		    .addFields(
+		        { name: "ban", value: `` },
+		        { name: "kick", value: `` },
+		        { name: "lock", value: `` },
+		        { name: "unlock", value: `` }
+		    )
+		   msg.edit(embed);
+		})
+		
+		coletor1.on('collect', c => {
+		    let embed = new Discord.MessageEmbed()
+		    .setTitle("Central de comandos")
+		    .setDescription("Economia")
+		    .setColor('#8A2BE2')
+		    .addFields(
+		        { name: "daily", value: `em desemvolvimento` },
+		        { name: "work", value: `em desemvolvimento` },
+		        { name: "dep all", value: `em desemvolvimento` },
+		        { name: "rank", value: `em desemvolvimento` },
+		        { name: "carteira", value: `em desemvolvimento` },
+		        { name: "aposta", value: `em desemvolvimento` },
+		        { name: "loja", value: `em desemvolvimento` }
+		    )
+		   msg.edit(embed);
+		})
+		coletor2.on('collect', c => {
+		    
+		})
     })
 }
