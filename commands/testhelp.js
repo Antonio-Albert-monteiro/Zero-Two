9894029f7122b8053a697f116f98213a110d9509
Antonio Albert <antonioalbert349@gmail.com>
2021-04-24 16:48:45 -0300

    update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -1,5 +1,14 @@
 const Discord = require("discord.js");
 
 exports.run = async (message, args) => {
-    
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Central de comandos")
+    .setDescription("
+        escolha a cartegoria:
+     🛠 moderação
+	 💰 Economia
+	 📡 Info
+	 😂 Diversão
+    ")
+    .setColor('#8A2BE2')
 }
