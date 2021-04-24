f3b417c1bb57fef9793579e12f942962289363c2
Antonio Albert <antonioalbert349@gmail.com>
2021-04-24 16:58:40 -0300

    update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -11,4 +11,18 @@ exports.run = async (message, args) => {
 	 😂 Diversão
     ")
     .setColor('#8A2BE2')
+    
+    message.channel.send(embed).then(msg => {
+        msg.react('🛠');
+        msg.react('💰');
+        msg.react('📡');
+        msg.react('😂');
+        
+        let filtro1 = (r, u) => r.emoji.name === '🛠' && u.id === message.author.id;
+		let filtro2 = (r, u) => r.emoji.name === '💰' && u.id === message.author.id;
+		let filtro3 = (r, u) => r.emoji.name === '📡' && u.id === message.author.id;
+		let filtro4 = (r, u) => r.emoji.name === '😂' && u.id === message.author.id;
+		
+		
+    })
 }
